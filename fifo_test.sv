af0e82a9ea5b703bbad3cc82642d28d0f29aadcb
MeghanaRBhat <115388361+MeghanaRBhat@users.noreply.github.com>
2023-09-14 15:54:27 +0530

    Create fifo_test.sv

---
+++ fifo_test.sv
@@ -0,0 +1,28 @@
+ `include "fifo_environment.sv"
+`include "my_sequence.sv"
+
+
+class fifo_test extends uvm_test;
+  my_sequence seq;
+  fifo_environment env;
+  `uvm_component_utils(fifo_test)
+
+  function new(string name = "fifo_test", uvm_component parent);
+    super.new(name, parent);
+  endfunction
+
+ virtual function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    seq = my_sequence::type_id::create("seq", this);
+    env = fifo_environment::type_id::create("env", this);
+  endfunction
+
+ virtual task run_phase(uvm_phase phase);
+    phase.raise_objection(this);
+    seq.start(env.agt.seqr);
+    phase.drop_objection(this);
+    phase.phase_done.set_drain_time(this, 100);
+  endtask
+
+endclass
+
