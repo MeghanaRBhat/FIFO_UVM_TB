2f18f6830cc24a7d8fa3fb7aa57a4f5723c593dd
MeghanaRBhat <115388361+MeghanaRBhat@users.noreply.github.com>
2023-09-15 15:13:36 +0530

    Update fifo_scoreboard.sv

--- fifo_scoreboard.sv
+++ fifo_scoreboard.sv
@@ -11,15 +11,16 @@ virtual function void build_phase(uvm_phase phase);
   super.build_phase(phase);
 endfunction
 
-int queue[$];
+  bit [127:0] queue[$];
 
 function void write(input sequence_item item_got);
- bit [7:0] data;
+  bit [127:0] data;
  if(item_got.i_wren == 1)begin
    queue.push_back(item_got.i_wrdata);
    `uvm_info("write Data", $sformatf("i_wren: %0b i_rden: %0b      i_wrdata : %0h o_full: %0b",item_got.i_wren, item_got.i_rden,item_got.i_wrdata , item_got.o_full), UVM_LOW);
     end
- else if (item_got.i_rden == 'b1)begin
+ 
+  if (item_got.i_rden == 'b1)begin
       if(queue.size() >= 1)begin
         data = queue.pop_front();
         `uvm_info("Read Data", $sformatf("data: %0h o_rddata: %0h o_empty: %0b",data, item_got.o_rddata, item_got.o_empty), UVM_LOW);
